d351ab761c0c57f84e3bb00cdf701b72f665404e
AdamuAbba <abbaadamu302@gmail.com>
2023-10-13 11:33:06 +0100

    chore: cleanup

---
+++ src/components/exchange/ExchangeRateCalculator/ExchangeRateCalculator.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import ReactiveButton from "reactive-button";
+import { useGetCoinQuery } from "../../../services/endpoints/v3";
+import { IButtonState } from "../../../utils/global/types";
+
+const ExchangeRateCalculator = () => {
+	const [buttonState, setButtonState] = useState<IButtonState>("idle");
+	// const { data,isLoading } = useGetCoinQuery();
+	// const CoinDataOBJ = Object.keys(data?.data?.rates) as [];
+	// console.log(CoinDataOBJ);
+
+	return (
+		<div className="bg-white lg:w-96 w-[90%] py-10 rounded-md drop-shadow-xl mt-8 space-y-7 justify-center items-center flex flex-col">
+			<div className="w-[90%]">
+				<input
+					placeholder="coin"
+					className="w-[67%] pl-2 rounded-l-md h-10 border-t border-b border-l border-black"
+				/>
+				<select className="border bg-royal text-white text-xs text-shadow-sm shadow-black h-10 w-[30%] rounded-r-md border-black">
+					<option>BUSD</option>
+				</select>
+			</div>
+			<div className="w-[90%]">
+				<input
+					placeholder="coin"
+					className="w-[67%] pl-2 rounded-l-md h-10 border-t border-b border-l border-black"
+				/>
+				<select className="border bg-royal text-white text-xs text-shadow-sm shadow-black h-10 w-[30%] rounded-r-md border-black">
+					<option>BUSD</option>
+				</select>
+			</div>
+			<ReactiveButton
+				style={{
+					borderRadius: 18,
+				}}
+				buttonState={buttonState}
+				idleText="Submit"
+				loadingText="Loading"
+				successText="Done"
+				errorText="Error"
+				type="submit"
+				size="large"
+				shadow
+			/>
+		</div>
+	);
+};
+
+export default ExchangeRateCalculator;
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -6,14 +6,29 @@ export const useAuth = () => {
 	const location = useLocation();
 	return useEffect(() => {
 		const authToken = sessionStorage.getItem("auth_token");
-		if (authToken) {
+		if (authToken && location.pathname === "/coin") {
 			navigate("/coin");
 		}
-		if (!authToken && location.pathname !== "/signup") {
+		if (authToken && location.pathname === "/") {
+			navigate("/coin");
+		}
+		if (authToken && location.pathname === "/signup") {
+			navigate("/coin");
+		}
+		if (authToken && location.pathname === "/coin/exchange-rate") {
+			navigate("/coin/exchange-rate");
+		}
+		if (!authToken && location.pathname !== "/coin") {
+			navigate("/");
+		}
+		if (!authToken && location.pathname !== "/coin/exchange-rate") {
 			navigate("/");
 		}
 		if (!authToken && location.pathname === "/signup") {
-			return;
+			navigate("/signup");
+		}
+		if (!authToken && location.pathname === "/") {
+			navigate("/");
 		}
 	}, [navigate, location.pathname]);
 };
